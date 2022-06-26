b6214523abbb6b856f60e6e8c4940d6a128db990
Ajay-AY <101260953+Ajay-AY@users.noreply.github.com>
2022-06-26 20:00:01 +0530

    Delete WPTAssighnments directory

--- WPTAssighnments/Assignment2/Nodejs/SeriousAssignment/q1.js
+++
@@ -1,7 +0,0 @@
-function previous(num) {
-  let rev = num - 1;
-  return rev;
-}
-
-let temp = previous(22);
-console.log("Previous num  => " + temp);
--- WPTAssighnments/Assignment2/Nodejs/SeriousAssignment/use.js
+++
@@ -1,2 +0,0 @@
-let lb = require("./lib");
-console.log("Previous num from lib file is => " + lb.f1(55));
