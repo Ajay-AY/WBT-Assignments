c8a9e75d541aa3f96d5ea6a463c355e04f50ba80
Ajay-AY <101260953+Ajay-AY@users.noreply.github.com>
2022-06-26 20:06:54 +0530

    Add files via upload

---
+++ Wpt-Assignments/Assignment2/lib.js
@@ -0,0 +1,7 @@
+exports.f1 = function previous(num) {
+  let rev = num - 1;
+  return rev;
+};
+
+// let temp = previous(22);
+// console.log("Previous num  => " + temp);
---
+++ Wpt-Assignments/Assignment2/q1.js
@@ -0,0 +1,7 @@
+function previous(num) {
+  let rev = num - 1;
+  return rev;
+}
+
+let temp = previous(22);
+console.log("Previous num  => " + temp);
